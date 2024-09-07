6e3aa16f6891c0f2cc2ee7e0ac17e9409ed7dc84
Que0ta <serg.dubetsky@gmail.com>
2024-09-07 22:23:24 +0300

    started the project (test render)

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.render('index.ejs');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
