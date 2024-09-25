9d9a4b23bea40c94ab666ce2216009f53210c38a
Que0ta <serg.dubetsky@gmail.com>
2024-09-25 23:22:36 +0300

    adaptibility test #2 + added paths

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+var currentPage = '';
 
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.render('index.ejs');
 });
+
+app.get('/projects', (req,res) => {
+    res.render('projects.ejs');
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.ejs');
+});
+
+app.get('/email', (req,res) => {
+    res.render('email.ejs');
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
