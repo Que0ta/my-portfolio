a020e62402a4e03c1eeba647ef241471e2382ecf
Que0ta <serg.dubetsky@gmail.com>
2025-08-07 00:23:36 +0300

    added message-box

---
+++ public/js/wavey.js
@@ -0,0 +1,48 @@
+const okeey = document.querySelector(".okeey");
+const offAnimation = document.querySelector(".neutral");
+
+const animationWindow = document.querySelector(".message");
+const wavesAnimation = document.querySelector(".waves");
+
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+function getCookie(name) {
+  const cname = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookies = decodedCookie.split(";");
+  for (let c of cookies) {
+    c = c.trim();
+    if (c.indexOf(cname) === 0) {
+      return c.substring(cname.length, c.length);
+    }
+  }
+  return null;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const saved = getCookie("animations");
+  console.log(saved);
+  if (saved === 'true') {
+    console.log(wavesAnimation.attributes);
+  } else if (saved === 'false') {
+    wavesAnimation.setAttribute("hidden", "");
+  } else if( saved === null ){
+    animationWindow.removeAttribute("hidden");
+  }
+});
+
+function offAnimationBlock() {
+  wavesAnimation.setAttribute("hidden", "");
+  animationWindow.setAttribute("hidden", "");
+  setCookie("animations", false, 30);
+}
+function onAnimationBlock() {
+  wavesAnimation.removeAttribute("hidden");
+  animationWindow.setAttribute("hidden", "");
+  setCookie("animations", true, 30);
+}
